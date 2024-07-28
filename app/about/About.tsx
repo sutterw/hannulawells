624ee362a087dbce7fbd967e5f58a52e53532a6f
Leo Harada <leotharada@gmail.com>
2024-07-27 19:57:16 -0700

    Complete aboutme page for desktop and mobile.

--- app/about/About.tsx
+++ app/about/About.tsx
@@ -11,7 +11,7 @@ const About = () => {
     return (
         <div className="relative flex flex-col items-center justify-center min-h-screen text-white overflow-hidden">
             <div
-                className={`absolute inset-0 bg-cover bg-center transition-transform duration-1000 ${
+                className={`absolute inset-0 bg-cover bg-center transition-transform duration-[4000ms] ${
                     zoom ? "scale-[3]" : ""
                 }`}
                 style={{ backgroundImage: "url('/mountains.png')" }}
@@ -19,31 +19,36 @@ const About = () => {
             <div className="absolute inset-x-0 top-0 h-1/4 bg-gradient-to-b from-black to-transparent"></div>
             <div className="absolute inset-x-0 bottom-0 h-1/4 bg-gradient-to-t from-black to-transparent"></div>
 
-            <div className="relative z-10 flex flex-col items-center justify-center">
+            <div className="relative z-10 flex flex-col items-center justify-center h-full text-center">
                 <h1
-                    className={`absolute text-9xl flex justify-center cursor-pointer transition duration-[2000ms] ${
-                        zoom ? "opacity-0 scale-[30]" : ""
+                    className={`absolute text-4xl sm:text-7xl lg:text-9xl cursor-pointer transition duration-[4000ms] ${
+                        zoom ? "opacity-0 scale-[100]" : ""
                     }`}
                     onClick={handleZoom}
+                    style={{ whiteSpace: "nowrap" }}
                 >
                     Why pe
-                    <span className="inline-block transform duration-[2000ms] group-hover:scale-[30]">
+                    <span
+                        className={`inline-block transition-transform duration-[4000ms] ${
+                            zoom ? "scale-[30]" : ""
+                        }`}
+                    >
                         o
                     </span>
-                    ple <span className="italic ml-2"> first</span>?
+                    ple <span className="italic">first</span>?
                 </h1>
                 <div
-                    className={`flex flex-col gap-8 text-center text-3xl transition-all duration-[2000ms] px-[5rem] shadow-custom ${
+                    className={`flex flex-col gap-8 transition-all duration-[3000ms] px-4 md:px-[5rem] text-lg md:text-3xl mb-[20%] lg:mb-[15%] xl:mb-[10%] ${
                         zoom
-                            ? "transform scale-100 opacity-100"
+                            ? "transform scale-100 opacity-100 mt-[40%] sm:mt-[20%] lg:mt-[15%] xl:mt-[10%]"
                             : "transform scale-0 opacity-0"
                     }`}
                 >
                     <p>
                         “People first” is the core of who we are, what we stand
                         for, and how we believe we can bring the best service to
-                        the people. No matter who is it, people always come
-                        first. People are the life blood of our organizations,
+                        the people. No matter who it is, people always come
+                        first. People are the lifeblood of our organizations,
                         the reason for our successes, and the catalysts for a
                         better future.
                     </p>
@@ -56,14 +61,14 @@ const About = () => {
                     <p>
                         With our focus on the people first, we have been given
                         the opportunity to help our clients in the ways they
-                        want to be helped. As a staple of our business we
+                        want to be helped. As a staple of our business, we
                         believe all clients deserve honest pricing and quality
                         engineering. Not just engineers who know how to get the
                         job but engineers who love to help people get the job
                         done.
                     </p>
                     <p>
-                        We are your dedicated cyber security team and understand
+                        We are your dedicated cybersecurity team and understand
                         that every security program starts with the people, your
                         people.
                     </p>
