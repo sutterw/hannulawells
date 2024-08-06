714687a7027dd2b7e349946057a10a6cdea89b44
Leo Harada <leotharada@gmail.com>
2024-08-05 22:46:56 -0700

    updated midsection of homepage.

--- app/home/HackersNeverStop.tsx
+++ app/home/HackersNeverStop.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const HackersNeverStop = () => {
     return (
         <section className="flex justify-center w-full h-auto my-[15%] relative">
-            <h1 className="absolute z-10 left-[27%] lg:text-[3rem] xl:text-[4rem] text-white">
+            <h1 className="absolute z-10 left-[27%] sm:text-[1.5rem] md:text-[2rem] lg:text-[2.5rem] xl:text-[3.5rem] 2xl:text-[4rem] text-white">
                 Hackers. Never. Stop.
             </h1>
-            <div className="relative w-[65%] h-auto rounded-[10%] overflow-hidden">
+            <div className="relative w-[80%] lg:w-[60%] h-auto rounded-[10%] overflow-hidden">
                 <video
                     preload="none"
                     autoPlay
@@ -21,7 +21,7 @@ const HackersNeverStop = () => {
             <img
                 src="/hackerstats.svg"
                 alt="hacker statistics by hannulawells"
-                className="absolute right-[5%] w-[25%]"
+                className="absolute right-[5%] lg:right-[8%] w-[25%]"
             />
         </section>
     );
--- app/home/PeopleMatter.tsx
+++ app/home/PeopleMatter.tsx
@@ -6,19 +6,19 @@ const PeopleMatter = () => {
             <img
                 src="/whiteW.svg"
                 alt="white hannulawells logo on homepage"
-                className="w-[20%] h-auto mt-32"
+                className="w-[40%] lg:w-[20%] h-auto mt-32"
             />
-            <h1 className="text-[#8E8E8E] text-[4rem] border-y-2 border-[#1C1C2D] py-4 px-24">
+            <h1 className="font-raleway text-[#8E8E8E] text-[2rem] sm:text-[4rem] border-y-2 border-[#1C1C2D] py-4 md:px-24">
                 Your <span className="text-white">people</span> matter to us.
             </h1>
-            <p className="text-center text-[1.75rem] w-[60%] leading-10 text-white opacity-85">
+            <p className="text-center text-[1.2rem] sm:text-[1.75rem] w-[90%] md:w-[60%] leading-10 text-white opacity-85">
                 We prioritize people over technology, offering value-driven
                 cybersecurity services tailored to meet our clients' unique
                 needs. With a client-centric approach, we strive to understand
                 your challenges and deliver comprehensive solutions to improve
                 your security posture and protect what matters most to you.
             </p>
-            <p className="text-white text-[1.75rem] opacity-85 border-y-2 border-[#1C1C2D] ">
+            <p className="font-raleway text-white text-[1.75rem] border-y-2 border-[#1C1C2D] ">
                 HannulaWells
             </p>
         </section>
