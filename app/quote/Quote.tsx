296c7598f989b01f52ec4db40080f5afb6443890
Leo Harada <leotharada@gmail.com>
2024-08-11 23:26:00 -0700

    added gradient border to quote form

--- app/quote/Quote.tsx
+++ app/quote/Quote.tsx
@@ -87,7 +87,7 @@ const Quote = (props: Props) => {
             <div className="relative flex flex-col w-full mx-auto lg:p-4 my-20">
                 <div className="flex flex-1 justify-center items-center md:p-8 lg:p-4">
                     <form
-                        className="w-full flex flex-col items-center text-black font-semibold p-6 md:p-8 lg:p-8 xl:max-w-[65%] lg:w-[70%] rounded-2xl shadow-lg"
+                        className="w-full flex flex-col items-center text-black font-semibold p-6 md:p-8 lg:p-8 xl:max-w-[65%] lg:w-[70%] rounded-2xl shadow-lg gradient-border"
                         onSubmit={handleSubmit}
                         style={{
                             minHeight: "100vh",
@@ -490,6 +490,43 @@ const Quote = (props: Props) => {
                         </div>
                         <SubmitBtn />
                     </form>
+                    <style jsx>{`
+                        .gradient-border {
+                            position: relative;
+                            border-radius: 1rem;
+                            overflow: hidden;
+                        }
+
+                        .gradient-border::before {
+                            content: "";
+                            position: absolute;
+                            top: 0;
+                            left: 0;
+                            right: 0;
+                            bottom: 0;
+                            border-radius: 1rem;
+                            padding: 1px;
+                            background: linear-gradient(
+                                135deg,
+                                rgba(144, 144, 144, 1),
+                                rgba(255, 255, 255, 0) 10%,
+                                rgba(255, 255, 255, 0) 50%,
+                                rgba(255, 255, 255, 0) 90%,
+                                rgba(144, 144, 144, 1)
+                            );
+                            -webkit-mask: linear-gradient(#fff 0 0) content-box,
+                                linear-gradient(#fff 0 0);
+                            -webkit-mask-composite: destination-out;
+                            mask-composite: exclude;
+                            z-index: 0;
+                            pointer-events: none;
+                        }
+
+                        .gradient-border > * {
+                            position: relative;
+                            z-index: 1;
+                        }
+                    `}</style>
                 </div>
             </div>
         </div>
