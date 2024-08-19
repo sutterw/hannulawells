fcf6c19c1477846d0a96be78653eac747eecdfd8
Leo Harada <leotharada@gmail.com>
2024-08-18 22:15:28 -0700

    effect added to logo

--- app/home/PeopleMatter.tsx
+++ app/home/PeopleMatter.tsx
@@ -1,12 +1,45 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 
 const PeopleMatter = () => {
+    const [isVisible, setIsVisible] = useState(false);
+    const sectionRef = useRef<HTMLOptionElement>(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                    observer.disconnect(); // Stop observing after the element is in view
+                }
+            },
+            { threshold: 0.1 } // Adjust threshold as needed
+        );
+
+        if (sectionRef.current) {
+            observer.observe(sectionRef.current);
+        }
+
+        return () => {
+            if (sectionRef.current) {
+                observer.unobserve(sectionRef.current);
+            }
+        };
+    }, []);
+
     return (
-        <section className="flex flex-col items-center w-full h-full gap-12">
+        <section
+            ref={sectionRef}
+            className={`flex flex-col items-center w-full h-full gap-12 transition-opacity duration-1000 ${
+                isVisible ? "fade-in" : ""
+            }`}
+        >
             <img
                 src="/whiteW.svg"
                 alt="white hannulawells logo on homepage"
-                className="w-[40%] lg:w-[20%] h-auto mt-32"
+                className={`w-[40%] lg:w-[20%] h-auto mt-32 ${
+                    isVisible ? "fade-in-logo" : ""
+                }`}
             />
             <h1 className="font-raleway text-[#8E8E8E] text-[2rem] sm:text-[4rem] border-y-2 border-[#1C1C2D] py-4 md:px-24">
                 Your <span className="text-white">people</span> matter to us.
